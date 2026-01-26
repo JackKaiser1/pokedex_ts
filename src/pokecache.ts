1191c1b95824d18e82c50908ad25fd3db1e46a21
jackkaiser1 <jackkaiser78@gmail.com>
2026-01-26 15:09:25 -0500

    Created Cache class

---
+++ src/pokecache.ts
@@ -0,0 +1,42 @@
+export type CacheEntry<T> = {
+    createdAt: number;
+    val: T;
+}
+
+export class Cache {
+    #cache = new Map<string, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+    #interval: number;
+
+    constructor(num: number) {
+        this.#interval = num;
+        this.#startReapLoop();
+    }
+
+    add<T>(key: string, val: T) {
+        this.#cache.set(key, {createdAt: Date.now(), val: val});
+    }
+
+    get<T>(key: string) {
+        return this.#cache.get(key);
+    }
+
+    #reap() {
+        for (const key of this.#cache.keys()) {
+            const cacheObj = this.#cache.get(key);
+            if (!cacheObj) continue;
+            else if (cacheObj.createdAt - this.#interval > this.#interval) {
+                this.#cache.delete(key);
+            }
+        }
+    }
+
+    #startReapLoop() {
+        this.#reapIntervalId = setInterval(this.#reap, this.#interval);
+    }
+
+    stopReapLoop() {
+        clearInterval(this.#reapIntervalId);
+        this.#reapIntervalId = undefined;
+    }
+}
